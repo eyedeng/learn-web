699ef33375ef04a1a245c0fd0b7a752af17ba7b3
dengyan <dengyan1912@outlook.com>
2020-04-29 18:24:37 +0800

    update 部分加密

--- z/en-de.js
+++ z/en-de.js
@@ -1,8 +1,13 @@
+// 访问时恢复加密内容，除了加密文件，好像还可躲审查
+
 window.onload = function(){
-    
     const key = parseInt(prompt("key number:", 0));
     const zh_sta = 0x4e00; 
     const zh_end = 0x9fa5 + key;
+
+    const no_code_part = false  // 非加密部分 标志
+    const no_code_s = 8594; 
+    const no_code_e = 8592;
 
     // let root = $("body").firstElementChild;
     let root = document.getElementsByTagName("body")[0].firstElementChild
@@ -16,9 +21,16 @@ window.onload = function(){
             let code = node.textContent,t = "";
             for(let i=0; i<code.length; i++){
                 let n = code.charCodeAt(i);
-                if(n >= zh_sta && n <= zh_end){
+                if(!no_code_part && n >= zh_sta && n <= zh_end){
                     t += String.fromCharCode(n - key);
-                }else{
+                }else if(n == no_code_s || n == no_code_e){
+                    if(n == no_code_s){
+                        no_code_part = true;
+                    }else{
+                        no_code_part = false;
+                    }
+                }
+                else{
                     t += code[i];
                 }
             }
--- z/en-de.py
+++ z/en-de.py
@@ -1,21 +1,33 @@
+import sys
 
-def main():
+
+def main(argv):
     """
     中文Unicode编码，凯撒加密，简单保护私人文件
     """
-    file_name = input("File name: ")
-    enc_or_dec = input("Encode or Decode File(e/d): ")
-    is_enc = False
-    if enc_or_dec[0] == 'e':
-        is_enc = True
-    key = int(input("Secret key number: "))
+    if len(argv) > 0:
+        file_name, key, is_enc = argv[0], int(argv[1]), True
+        if len(argv) == 3 and argv[2][0] == 'd':
+            is_enc = False
+    else:
+        file_name = input("File name: ")
+        key = int(input("Secret key number: "))
+        enc_or_dec = input("Encode or Decode File(e/d): ")
+        is_enc = False
+        if enc_or_dec[0] == 'e':
+            is_enc = True
     zh_sta, zh_end = 0x4e00, 0x9fa5 + key
     code = ""
 
+    no_code_part = False  # 非加密部分 标志
+    no_code_s, no_code_e = 8594, 8592
+
     with open(file_name, "r", encoding="utf-8") as f:
         for c in f.read():
-            if zh_sta <= ord(c) <= zh_end:
+            if not no_code_part and zh_sta <= ord(c) <= zh_end:
                 code += chr(ord(c) + key) if is_enc else chr(ord(c) - key)
+            elif ord(c) == no_code_s or ord(c) == no_code_e:
+                no_code_part = True if ord(c) == no_code_s else False
             else:
                 code += c
 
@@ -25,4 +37,4 @@ def main():
 
 
 if __name__ == "__main__":
-    main()
+    main(sys.argv[1:])
